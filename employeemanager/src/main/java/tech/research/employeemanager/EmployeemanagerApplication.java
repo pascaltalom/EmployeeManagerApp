4ed85bc4445f02654226e0fc2b269dbbf4a13f1b
pascaltalom <pascal.turbo9@gmail.com>
2024-05-11 14:49:22 +0200

    Model class Updated
    created constructors, getters and setters

--- employeemanager/src/main/java/tech/research/employeemanager/EmployeemanagerApplication.java
+++ employeemanager/src/main/java/tech/research/employeemanager/EmployeemanagerApplication.java
@@ -2,10 +2,9 @@ package tech.research.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
--- employeemanager/src/main/java/tech/research/employeemanager/Models/employeeModel.java
+++ employeemanager/src/main/java/tech/research/employeemanager/Models/employeeModel.java
@@ -1,14 +1,102 @@
 package tech.research.employeemanager.Models;
+
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-public class employeeModel implements Serializable{
-    private Long id;
-    private String name;
-    private String email;
-    private String jobTitle;
-    private String phone;
-    private String imageUrl;
-    private String employeeCode;
+@Entity
+public class employeeModel implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(nullable = false, updatable = false)
+  private Long id;
+  private String name;
+  private String email;
+  private String phone;
+  private String jobTitle;
+  private String imageUrl;
+  @Column(nullable = false, updatable = false)
+  private String employeeCode;
 
+public employeeModel() {}
+
+public employeeModel(String name, String email, String phone, String jobTitle, String imageUrl, String employeeCode) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.jobTitle = jobTitle;
+    this.imageUrl = imageUrl;
+    this.employeeCode = employeeCode;
+}
+
+public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getPhone() {
+    return phone;
+}
+
+public void setPhone(String phone) {
+    this.phone = phone;
+}
+
+public String getJobTitle() {
+    return jobTitle;
+}
+
+public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+}
+
+public String getImageUrl() {
+    return imageUrl;
+}
+
+public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+}
+
+public String getEmployeeCode() {
+    return employeeCode;
+}
+
+public void setEmployeeCode(String employeeCode) {
+    this.employeeCode = employeeCode;
+}
+
+@Override
+public String toString() {
+    return "employeeModel [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", jobTitle="
+            + jobTitle + ", imageUrl=" + imageUrl + "]";
+}
+
+
+
+  
+    
 }
