cf72ca8cc1166fbd3118e03d2e35e1bdb70a5e4a
pascaltalom <pascal.turbo9@gmail.com>
2024-05-11 14:21:11 +0200

    Model Class
    Created the model Class for all employees

--- employeemanager/src/main/java/tech/research/employeemanager/EmployeemanagerApplication.java
+++ employeemanager/src/main/java/tech/research/employeemanager/EmployeemanagerApplication.java
@@ -2,8 +2,10 @@ package tech.research.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
---
+++ employeemanager/src/main/java/tech/research/employeemanager/Models/employeeModel.java
@@ -0,0 +1,14 @@
+package tech.research.employeemanager.Models;
+import java.io.Serializable;
+
+
+public class employeeModel implements Serializable{
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+
+}
